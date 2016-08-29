b676ebe4cdf386b013f41d191f7443e55d10923b
mat35770 <mat35770@hotmail.fr>
2016-08-29 16:21:18 -0300

    modification of add task

--- app/src/main/java/com/example/mathieu/waitandgo/AddTaskActivity.java
+++ app/src/main/java/com/example/mathieu/waitandgo/AddTaskActivity.java
@@ -1,12 +1,24 @@
 package com.example.mathieu.waitandgo;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 /**
  * Created by Mathieu on 29/08/2016.
  */
 
-public class AddTaskActivity extends AppCompatActivity {
+public class AddTaskActivity extends Activity {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+
+    }
 
 }
