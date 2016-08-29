d9e8c0e381dc9c71447ce0d601f40c21c97e7532
mat35770 <mat35770@hotmail.fr>
2016-08-29 12:18:19 -0300

    Modification menu

--- app/src/main/java/com/example/mathieu/waitandgo/MainActivity.java
+++ app/src/main/java/com/example/mathieu/waitandgo/MainActivity.java
@@ -80,17 +80,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_list) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_group) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_group_add) {
 
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
---
+++ app/src/main/java/com/example/mathieu/waitandgo/SearchableActivity.java
@@ -0,0 +1,8 @@
+package com.example.mathieu.waitandgo;
+
+/**
+ * Created by Mathieu on 29/08/2016.
+ */
+
+public class SearchableActivity {
+}
