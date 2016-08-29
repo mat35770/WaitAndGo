d8c766155ccf7e40170023d1aa3d8b1f3152a7f5
mat35770 <mat35770@hotmail.fr>
2016-08-29 20:16:32 -0300

    update add_task

--- app/src/main/java/com/example/mathieu/waitandgo/AddTaskActivity.java
+++ app/src/main/java/com/example/mathieu/waitandgo/AddTaskActivity.java
@@ -1,24 +1,27 @@
 package com.example.mathieu.waitandgo;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.Toolbar;
+import android.view.Menu;
 
 /**
  * Created by Mathieu on 29/08/2016.
  */
 
-public class AddTaskActivity extends Activity {
+public class AddTaskActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
-
+        android.support.v7.widget.Toolbar toolbar_task = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar_task);
+        setSupportActionBar(toolbar_task);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_add_task_drawer, menu);
+        return true;
+    }
 }
