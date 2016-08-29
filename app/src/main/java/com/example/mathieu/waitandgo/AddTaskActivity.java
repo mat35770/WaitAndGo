2472da9bde7ee299b0723d585f003064bb8c6986
mat35770 <mat35770@hotmail.fr>
2016-08-29 12:54:03 -0300

    creation of add task design

---
+++ app/src/main/java/com/example/mathieu/waitandgo/AddTaskActivity.java
@@ -0,0 +1,12 @@
+package com.example.mathieu.waitandgo;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mathieu on 29/08/2016.
+ */
+
+public class AddTaskActivity extends AppCompatActivity {
+
+
+}
