2f2e3b7194a70720bf272b3e7c16db38ce03e10d
mat35770 <mat35770@hotmail.fr>
2016-08-29 21:09:27 -0300

    add button back

--- app/src/main/java/com/example/mathieu/waitandgo/AddTaskActivity.java
+++ app/src/main/java/com/example/mathieu/waitandgo/AddTaskActivity.java
@@ -1,8 +1,14 @@
 package com.example.mathieu.waitandgo;
 
+import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ButtonBarLayout;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Mathieu on 29/08/2016.
@@ -12,6 +18,7 @@ public class AddTaskActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState){
+        //Creation of the view to add a task
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
         android.support.v7.widget.Toolbar toolbar_task = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar_task);
@@ -24,4 +31,21 @@ public class AddTaskActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.activity_add_task_drawer, menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_back) {
+            Intent intent = new Intent(AddTaskActivity.this,MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (id == R.id.nav_valid){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
